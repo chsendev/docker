901c9b95e1ba974d4632990c4e994e4b7710f7d5
Chen Sen <87457607+chsendev@users.noreply.github.com>
2025-12-14 18:35:49 +0800

    Create model.go

---
+++ es/model.go
@@ -0,0 +1,22 @@
+package main
+
+// TbHotel undefined
+type TbHotel struct {
+	ID        int64  `json:"id" gorm:"id"`               // 酒店id
+	Name      string `json:"name" gorm:"name"`           // 酒店名称
+	Address   string `json:"address" gorm:"address"`     // 酒店地址
+	Price     int64  `json:"price" gorm:"price"`         // 酒店价格
+	Score     int64  `json:"score" gorm:"score"`         // 酒店评分
+	Brand     string `json:"brand" gorm:"brand"`         // 酒店品牌
+	City      string `json:"city" gorm:"city"`           // 所在城市
+	StarName  string `json:"star_name" gorm:"star_name"` // 酒店星级，1星到5星，1钻到5钻
+	Business  string `json:"business" gorm:"business"`   // 商圈
+	Latitude  string `json:"latitude" gorm:"latitude"`   // 纬度
+	Longitude string `json:"longitude" gorm:"longitude"` // 经度
+	Pic       string `json:"pic" gorm:"pic"`             // 酒店图片
+}
+
+// TableName 表名称
+func (*TbHotel) TableName() string {
+	return "tb_hotel"
+}
